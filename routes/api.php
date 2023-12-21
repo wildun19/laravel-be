2a84b15acfc378242d3eeb0ff2c10d502b7d6e09
wildun19 <wildanabdunnaseer19@gmail.com>
2023-12-22 00:00:24 +0700

    api_benners

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,12 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\BannerController;
 use App\Http\Controllers\Api\CallbackController;
-use App\Http\Controllers\Api\UploadController;
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\OrderController;
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\UploadController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -40,3 +41,4 @@ Route::post('midtrans/notification/handling', [CallbackController::class, 'callb
 
 Route::apiResource('categories', CategoryController::class);
 Route::apiResource('products', ProductController::class);
+Route::apiResource('banners', BannerController::class);
